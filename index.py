68567585f760133f1c137fa295beceda60e2567e
Ivan Vuong <vuongi2@uci.edu>
2025-05-12 18:17:36 -0700

    files

---
+++ index.py
@@ -0,0 +1,43 @@
+'''
+nltk library
+
+Not allowed to use Lucene, PyLucene, or ElasticSearch.
+
+Indexer:
+Create an inverted index for the given corpus with data structures designed by you.
+
+Tokens: all alphanumeric sequences in the dataset.
+Stop words: do not use stopping, i.e. use all words, even the frequently occurring ones.
+Stemming: use stemming for better textual matches. Suggestion: Porter stemming.
+Important words: Words in bold, in headings (h1, h2, h3), and in titles should be treated as more important than the other words.
+
+The inverted index is simply a map with the token as a key and a list of its corresponding postings.
+A posting is the representation of the token’s occurrence in a document.
+
+'''
+
+def create_inverted_index():
+    '''
+    Map with a token as a key and its list of its corresponding postings 
+
+    Example:
+
+    "the" : {DOCUMENTID/DOCUMENTNAME, TERM FREQUENCY}
+    '''
+    pass 
+
+def number_of_indexed_documents():
+    pass
+
+def number_of_unique_tokens():
+    pass
+
+def total_size():
+    pass 
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
